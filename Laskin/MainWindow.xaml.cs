c2151596d119c1bc33fe84a4619883509827a857
HeneDev <44865736+HeneDev@users.noreply.github.com>
2021-09-27 18:30:57 +0300

    Uploaded finished version

--- Laskin/MainWindow.xaml.cs
+++ Laskin/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,129 +21,70 @@ namespace Laskin
     /// </summary>
     public partial class MainWindow : Window
     {
-        Double result = 0;
-        Double tempResult = 0;
-        String operation = "";
-        bool enter_value = false;
+        /*
+         * To review:
+         * - operations with multiple power, sqrt etc.
+         */
+        double input = 0;
+        double cache = 0;
+        string operation = null;
+        bool hasResult = false;
+        bool hasCache = false;
 
         public MainWindow()
         {
             InitializeComponent();
         }
-        private void changeLabelText(Double result, Double tempResult)
+
+        private void btnNo_Click(object sender, RoutedEventArgs e)
         {
-            lblDisplay.Text = System.Convert.ToString(result) + " " + operation + " ";
-            lblDisplay.Text += tempResult.ToString() + " " + " = ";
-        }
+            Button button = sender as Button;
 
-        private void btn0_Click(object sender, RoutedEventArgs e)
-        {
-            if ((txtDisplay.Text == "0"))
-            {
-                txtDisplay.Text = "";
-            }
-
-            txtDisplay.Text = txtDisplay.Text + "0";
-            enter_value = true;
-        }
-
-        private void btn1_Click(object sender, RoutedEventArgs e)
-        {
-            if ((txtDisplay.Text == "0"))
-            {
-                txtDisplay.Text = "";
-            }
-
-            txtDisplay.Text = txtDisplay.Text + "1";
-            enter_value = true;
-        }
-
-        private void btn2_Click(object sender, RoutedEventArgs e)
-        {
-            if ((txtDisplay.Text == "0"))
-            {
-                txtDisplay.Text = "";
-            }
-
-            txtDisplay.Text = txtDisplay.Text + "2";
-            enter_value = true;
-        }
-
-        private void btn3_Click(object sender, RoutedEventArgs e)
-        {
-            if ((txtDisplay.Text == "0"))
-            {
-                txtDisplay.Text = "";
-            }
-
-            txtDisplay.Text = txtDisplay.Text + "3";
-            enter_value = true;
-        }
-
-        private void btn4_Click(object sender, RoutedEventArgs e)
-        {
-            if ((txtDisplay.Text == "0"))
-            {
-                txtDisplay.Text = "";
-            }
-
-            txtDisplay.Text = txtDisplay.Text + "4";
-            enter_value = true;
-        }
-
-        private void btn5_Click(object sender, RoutedEventArgs e)
-        {
-            if ((txtDisplay.Text == "0"))
-            {
-                txtDisplay.Text = "";
-            }
-
-            txtDisplay.Text = txtDisplay.Text + "5";
-            enter_value = true;
-        }
-
-        private void btn6_Click(object sender, RoutedEventArgs e)
-        {
-            if ((txtDisplay.Text == "0"))
-            {
-                txtDisplay.Text = "";
-            }
-
-            txtDisplay.Text = txtDisplay.Text + "6";
-            enter_value = true;
-        }
-
-        private void btn7_Click(object sender, RoutedEventArgs e)
-        {
             if (txtDisplay.Text == "0")
+                txtDisplay.Text = button.Content.ToString();
+            else if (hasResult)
             {
-                txtDisplay.Text = "";
+                // If after pressing the equals sign and getting the result, the user start inputting new number
+                // => Clear everything
+                equationDisplay.Text = null;
+                input = 0;
+                operation = null;
+                hasResult = false;
+                txtDisplay.Text = button.Content.ToString();
             }
-
-            txtDisplay.Text = txtDisplay.Text + "7";
-            enter_value = true;
+            else
+                txtDisplay.Text += button.Content.ToString();
         }
 
-        private void btn8_Click(object sender, RoutedEventArgs e)
+        private void btnPosNeg_Click(object sender, RoutedEventArgs e)
         {
-            if (txtDisplay.Text == "0")
+            if (hasResult)
             {
-                txtDisplay.Text = "";
+                txtDisplay.Text = (Convert.ToDouble(input) * -1.0).ToString();
+                equationDisplay.Text = null;
+                input = 0;
+                operation = null;
+                hasResult = false;
             }
-
-            txtDisplay.Text = txtDisplay.Text + "8";
-            enter_value = true;
+            txtDisplay.Text = (Convert.ToDouble(txtDisplay.Text) * -1.0).ToString();
         }
 
-        private void btn9_Click(object sender, RoutedEventArgs e)
+        private void btnComma_Click(object sender, RoutedEventArgs e)
         {
-            if (txtDisplay.Text == "0")
+            if (hasResult)
             {
-                txtDisplay.Text = "";
+                equationDisplay.Text = null;
+                input = 0;
+                operation = null;
+                hasResult = false;
             }
 
-            txtDisplay.Text = txtDisplay.Text + "9";
-            enter_value = true;
+            if (txtDisplay.Text == "0")
+                txtDisplay.Text = "0.";
+            else if (txtDisplay.Text.Contains("."))
+                txtDisplay.Text = txtDisplay.Text;
+            else
+                txtDisplay.Text += ".";
         }
 
         private void btnClearEntry_Click(object sender, RoutedEventArgs e)
@@ -152,202 +94,204 @@ namespace Laskin
 
         private void btnClear_Click(object sender, RoutedEventArgs e)
         {
+            // Clear everything
             txtDisplay.Text = "0";
-            result = 0;
-            lblDisplay.Text = "";
+            input = 0;
+            cache = 0;
+            operation = null;
+            equationDisplay.Text = null;
+            hasResult = false;
+            hasCache = false;
         }
 
         private void btnBackspace_Click(object sender, RoutedEventArgs e)
         {
-            if (txtDisplay.Text.Length > 0)
-            {
-                txtDisplay.Text = txtDisplay.Text.Remove(txtDisplay.Text.Length - 1, 1);
-            }
-
-            if (txtDisplay.Text == "")
-            {
+            if (txtDisplay.Text.Length > 1)
+                txtDisplay.Text = txtDisplay.Text.Substring(0, txtDisplay.Text.Length - 1);
+            else
                 txtDisplay.Text = "0";
-            }
-        }
-
-        private void btnPlus_Click(object sender, RoutedEventArgs e)
-        {
-            operation = "+";
-            result = Double.Parse(txtDisplay.Text);
-            txtDisplay.Text = "0";
-            lblDisplay.Text = System.Convert.ToString(result) + " " + operation + " ";
-            enter_value = false;
-        }
-
-        private void btnMinus_Click(object sender, RoutedEventArgs e)
-        {
-            operation = "-";
-            result = Double.Parse(txtDisplay.Text);
-            txtDisplay.Text = "0";
-            lblDisplay.Text = System.Convert.ToString(result) + " " + operation + " ";
-            enter_value = false;
-        }
-
-        private void btnTimes_Click(object sender, RoutedEventArgs e)
-        {
-            operation = "*";
-            result = Double.Parse(txtDisplay.Text);
-            txtDisplay.Text = "0";
-            lblDisplay.Text = System.Convert.ToString(result) + " " + operation + " ";
-            enter_value = false;
-        }
-
-        private void btnDivide_Click(object sender, RoutedEventArgs e)
-        {
-            operation = "/";
-            result = Double.Parse(txtDisplay.Text);
-            txtDisplay.Text = "0";
-            lblDisplay.Text = System.Convert.ToString(result) + " " + operation + " ";
-            enter_value = false;
         }
 
         private void btnEquals_Click(object sender, RoutedEventArgs e)
         {
-            switch(operation)
+            if (txtDisplay.Text != "0")
+            {
+                //3.
+                // You can't crash the program by erasing txtDisplay.Text
+                Calculate();
+                hasResult = true;
+                txtDisplay.Text = input.ToString();
+            }
+        }
+
+        private void btnPercent_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == null)
+            {
+                input = Convert.ToDouble(txtDisplay.Text) * 0.01;
+                equationDisplay.Text = txtDisplay.Text + "%";
+                txtDisplay.Text = input.ToString();
+            }
+            else
+            {
+                switch (operation)
+                {
+                    case "+":
+                        input += Convert.ToDouble(txtDisplay.Text) / 100 * input;
+                        break;
+                    case "-":
+                        input -= Convert.ToDouble(txtDisplay.Text) / 100 * input;
+                        break;
+                    case "*":
+                        input *= Convert.ToDouble(txtDisplay.Text) / 100;
+                        break;
+                    case "/":
+                        input /= Convert.ToDouble(txtDisplay.Text) / 100;
+                        break;
+                }
+
+                equationDisplay.Text = equationDisplay.Text + txtDisplay.Text + "%";
+                txtDisplay.Text = input.ToString();
+                hasResult = true;
+                operation = null;
+            }
+        }
+
+        private void Calculate()
+        {
+            if (input == 0)
+                equationDisplay.Text = null;
+
+            if (!hasCache)
+            {
+                cache = Convert.ToDouble(txtDisplay.Text);
+                equationDisplay.Text += txtDisplay.Text;
+            }
+
+            switch (operation)
             {
                 case "+":
-                    if (enter_value)
-                    {
-                        tempResult = Double.Parse(txtDisplay.Text);
-                    }
-                    changeLabelText(result, tempResult);
-                    result += tempResult;
-                    txtDisplay.Text = result.ToString();
-                    enter_value = false;
+                    input += cache;
                     break;
-
                 case "-":
-                    if (enter_value)
-                    {
-                        tempResult = Double.Parse(txtDisplay.Text);
-                    }
-                    changeLabelText(result, tempResult);
-                    result -= tempResult;
-                    txtDisplay.Text = result.ToString();
-                    enter_value = false;
+                    input -= cache;
                     break;
-
                 case "*":
-                    if (enter_value)
-                    {
-                        tempResult = Double.Parse(txtDisplay.Text);
-                    }
-                    changeLabelText(result, tempResult);
-                    result *= tempResult;
-                    txtDisplay.Text = result.ToString();
-                    enter_value = false;
+                    input *= cache;
                     break;
-
                 case "/":
-                    if (enter_value)
-                    {
-                        tempResult = Double.Parse(txtDisplay.Text);
-                    }
-                    changeLabelText(result, tempResult);
-                    result /= tempResult;
-                    txtDisplay.Text = result.ToString();
-                    enter_value = false;
-                    break;
-
-                case "Mod":
-                    if (enter_value)
-                    {
-                        tempResult = Double.Parse(txtDisplay.Text);
-                    }
-                    changeLabelText(result, tempResult);
-                    result %= tempResult;
-                    txtDisplay.Text = result.ToString();
-                    enter_value = false;
-                    break;
-
-                case "Exp":
-                    double i = Double.Parse(txtDisplay.Text);
-                    double j = result;
-                    txtDisplay.Text = Math.Exp(i * Math.Log(j * 4)).ToString();
+                    if (cache != 0)
+                        input /= cache;
                     break;
             }
-        }
-
-        private void btnComma_Click(object sender, RoutedEventArgs e)
-        {
-            if ((txtDisplay.Text == "0"))
-            {
-                txtDisplay.Text = "";
-            }
-
-            txtDisplay.Text = txtDisplay.Text + ",";
-        }
-
-        private void btnPosNeg_Click(object sender, RoutedEventArgs e)
-        {
-            result = Double.Parse(txtDisplay.Text);
-            result *= -1;
-            txtDisplay.Text = System.Convert.ToString(result);
-        }
-
-        private void btnModulus_Click(object sender, RoutedEventArgs e)
-        {
-            operation = "Mod";
-            result = Double.Parse(txtDisplay.Text);
-            txtDisplay.Text = "0";
-            lblDisplay.Text = System.Convert.ToString(result) + " " + operation + " ";
-            enter_value = false;
         }
 
         private void btn1x_Click(object sender, RoutedEventArgs e)
         {
-            double x;
-            x = Convert.ToDouble(1.0 / Convert.ToDouble(txtDisplay.Text));
-            txtDisplay.Text = System.Convert.ToString(x);
+            if (operation != null)
+            {
+                equationDisplay.Text = equationDisplay.Text + "1/" + txtDisplay.Text;
+                cache = 1 / Convert.ToDouble(txtDisplay.Text);
+                hasCache = true;
+                Calculate();
+                txtDisplay.Text = Convert.ToString(cache);
+                operation = null;
+            }
+            else
+            {
+                input = 1 / Convert.ToDouble(txtDisplay.Text);
+                equationDisplay.Text = equationDisplay.Text + "1/" + txtDisplay.Text;
+                txtDisplay.Text = Convert.ToString(input);
+            }
         }
 
         private void btnPower_Click(object sender, RoutedEventArgs e)
         {
-            operation = "^ 2";
-            result = Double.Parse(txtDisplay.Text);
-            txtDisplay.Text = (Math.Pow(result, 2)).ToString();
-            lblDisplay.Text = System.Convert.ToString(result) + " " + operation + " ";
-            result = Math.Pow(Double.Parse(txtDisplay.Text), 2);
-            enter_value = false;
-
+            if (operation != null)
+            {
+                equationDisplay.Text = equationDisplay.Text + txtDisplay.Text + "^2";
+                cache = Math.Pow(Convert.ToDouble(txtDisplay.Text), 2.0);
+                txtDisplay.Text = Convert.ToString(cache);
+                hasCache = true;
+                Calculate();
+                operation = null;
+            }
+            else
+            {
+                input = Math.Pow(Convert.ToDouble(txtDisplay.Text), 2.0);
+                equationDisplay.Text = txtDisplay.Text + "^2";
+                txtDisplay.Text = Convert.ToString(input);
+            }
         }
 
         private void btnSqrt_Click(object sender, RoutedEventArgs e)
         {
-            double sqrt = Double.Parse(txtDisplay.Text);
-            sqrt = Math.Sqrt(sqrt);
-            txtDisplay.Text = System.Convert.ToString(sqrt);
-            lblDisplay.Text = System.Convert.ToString(result) + " " + operation + " ";
-            enter_value = false;
+            if (operation != null)
+            {
+                equationDisplay.Text = equationDisplay.Text + "sqrt(" + txtDisplay.Text + ")";
+                cache = Math.Sqrt(Convert.ToDouble(txtDisplay.Text));
+                hasCache = true;
+                txtDisplay.Text = Convert.ToString(cache);
+                Calculate();
+                operation = null;
+            }
+            else
+            {
+                input = Math.Sqrt(Convert.ToDouble(txtDisplay.Text));
+                equationDisplay.Text = "sqrt(" + txtDisplay.Text + ")";
+                txtDisplay.Text = Convert.ToString(input);
+            }
         }
 
-        private void MenuStandardClick(object sender, RoutedEventArgs e)
+        private void btnArithmetic_Click(object sender, RoutedEventArgs e)
         {
-            this.Width = 330;
-            this.txtDisplay.Width = 310;
-            this.lblDisplay.Width = 310;
+            Button button = sender as Button;
+
+            if (operation == null)
+            {
+                equationDisplay.Text = txtDisplay.Text;
+                input = Convert.ToDouble(txtDisplay.Text);
+            }
+            else if (hasResult)
+            {
+                equationDisplay.Text = input.ToString();
+                hasResult = false;
+            }
+            else
+            {
+                //If there is already input & operation and the user presses arithmetic operator again
+                // => calculate the existing operation as input
+                Calculate();
+            }
+
+            operation = button.Content.ToString();
+            equationDisplay.Text += operation;
+            txtDisplay.Text = "0";
+
         }
 
         private void MenuScientificClick(object sender, RoutedEventArgs e)
         {
-            this.Width = 680;
-            this.txtDisplay.Width = 670;
-            this.lblDisplay.Width = 670;
+            ScientificWindow science = new ScientificWindow();
+            science.Show();
+            Close();
+        }
+
+        private void MenuStandardClick(object sender, RoutedEventArgs e)
+        {
+
         }
 
         private void MenuTemperatureClick(object sender, RoutedEventArgs e)
         {
-
+            TemperatureWindow temperature = new TemperatureWindow();
+            temperature.Show();
+            Close();
         }
 
         private void MenuExitClick(object sender, RoutedEventArgs e)
         {
-            this.Close();
+            Close();
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,9 @@
+# Calculator
+
+Calculator made in C#. Has scientific and temperature conversion modes.
+
+![Standard calculator](/Images/Standard.png)
+
+![Scientific calculator](/Images/Scientific.png)
+
+![Temperature conversion](/Images/Temperature.png)
